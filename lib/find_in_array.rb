b63937721128af531d19bdfa085e0e6d7b031763
confident-architect-8379 <s.kanagui@gmail.com>
2020-08-14 17:25:20 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -4,7 +4,8 @@ def find_element_index(array, value_to_find)
   while count < array.length do 
     if array [count] == value_to_find
       found_value_index = count 
-      count += 1 
     end 
-    
+    count += 1 
+  end  
+    found_value_index
 end
